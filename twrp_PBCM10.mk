a6751fa9c18fbc34ee143857e99983dae9b7cdfa
ghhccghk <2137610394@qq.com>
2022-04-10 07:55:54 +0800

    del gsi
    Signed-off-by: ghhccghk <2137610394@qq.com>

--- twrp_PBCM10.mk
+++ twrp_PBCM10.mk
@@ -11,7 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
